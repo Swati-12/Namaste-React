a3843e64ebb1417744c3b5aae3ba868d17b89990
Swati Saini <swatisatjiemail@gmail.com>
2023-07-23 18:45:42 +0530

    Episod 3

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
+    import React from "react";
+    import ReactDOM from "react-dom/client";
+    
+    
+    
     const heading = React.createElement("div" , { id:"parent"}, React.createElement("div" , {id:"child"},[
         React.createElement("h1",{id:"h1"} , "Hello H1 from React Js") ,
         React.createElement("h2" , {id:"h2"} , "Hello H2" )
     ]));
     console.log(heading);
+
+    const jsx = <h1 id="js"> This is JSX Example </h1>;
+    console.log(jsx);
+
+    const Title=()=>{
+        <h1 className="title"> "Hello I'm Title</h1>
+    };
+
+    const Fun =()=>{
+        <Title/>
+        return <h2>Hello Swati ! in the Coding World</h2>
+
+    };
+
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    root.render(<Fun/>);
+
+    
